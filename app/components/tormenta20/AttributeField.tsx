35b27100af15b09dd22761159de328739731b5db
LuanGB <luan.goncbs@gmail.com>
2025-03-22 15:48:45 -0300

    feat: add example component

---
+++ app/components/tormenta20/AttributeField.tsx
@@ -0,0 +1,20 @@
+import { AttrGoty } from '../../images/tormenta20/attr-goty'
+
+type AttributeFieldProps = {
+  label: string;
+  value: number;
+}
+
+const AttributeField = ({label, value}) => {
+  return (
+    <div className="flex w-fit h-fit justify-center">
+      <p className="mt-0.5 absolute text-lg font-bold">{label}</p>
+      <div className="flex w-fit h-fit justify-center items-center">
+        <AttrGoty />
+        <span className="mt-3 absolute text-4xl">{value}</span>
+      </div>
+    </div>
+  )
+}
+
+export default AttributeField
